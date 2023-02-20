e4819c7ed4f8f6333453bf0a6c5a0ae54809ff0e
Cyprian <cyprianwaclaw@gmail.com>
2023-02-20 14:16:34 +0100

    💄 Adding desktop view

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class=" bg-blue-50 h-screen">
     <NuxtPage />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,61 @@
 <template>
-  <div class="mt-5 mx-20 relative">
-    <h1 class="text-2xl text-blue-500 mb-5">Input</h1>
-    <form @submit.prevent="onSubmit">
-      <input
-        type="text"
-        v-model.trim="inputNumber"
-        placeholder="Wprowadź dowolne liczby"
-      />
-      <div v-if="inputNumber.split(',').length < 3 && inputNumber.length > 0">
-        <p>Wprowadź minimum 3 liczby</p>
+  <div class="pt-5 mx-20 grid place-items-center">
+    <div class="retangle">
+      <div class="flex flex-col justify-start mb-2">
+        <h1 class="text-2xl font-medium">
+          Znajdź liczbę, która <span class="primary font-2xl"> nie pasuje</span><br> do pozostałych
+        </h1>
+        <p class="mt-4">Wyprowadź minumum 3 liczby oddzielając je<br>przecinkami a aplikacja znajdzie liczbę,<br>która nie pasuje do pozostałych </p>
       </div>
-      <button
+      <form @submit.prevent="onSubmit">
+        <input
+          type="text"
+          v-model.trim="inputNumber"
+          placeholder="Twoje liczby"
+          autofocus
+        />
+        <button
         class="primary-button"
         type="submit"
         :disabled="inputNumber.split(',').length < 3 ? true : false"
-      >
+        >
         Wyślij
       </button>
-    </form>
-    <div class="mt-20 ">
-      <p>test</p>
-  {{ myNumber }}
+      <div v-if="inputNumber.split(',').length < 3 && inputNumber.length > 0">
+        <p class="text-des-error">Wprowadź 3 liczby</p>
+        </div>
+      </form>
+      <div v-if="myNumber" class="flex mt-12 columns-2 justify-between w-full">
+        <div  class=" ">
+          <p class="text-2xl font-medium">Niepasująca liczba:</p>
+          <p class="primary text-xl">{{ myNumber }}</p>
+        </div>
+        <div class="mt-1.5">
+          <button @click="onClick()" class="remove">Usuń wynik</button>
+        </div>
+    </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import {toNumber, findOutlier} from "@/utils/function"
-const inputNumber = ref("");
-const myNumber = ref<any>()
-const myNumber1 = ref<number []>([])
-
-
+import { toNumber, findOutlier } from "@/utils/function";
+const inputNumber = ref<any>("");
+const myNumber = ref<any>();
 
 function onSubmit() {
-
-  let array = inputNumber.value.split(',')
+  let array = inputNumber.value.split(",");
   let numberArray = array.map(toNumber);
 
- let number = findOutlier(numberArray);
-console.log(number)
+  let number = findOutlier(numberArray);
+  console.log(number);
 
- myNumber.value = numerOk(number)
+  myNumber.value = numerOk(number);
 }
 
+function onClick(){
+  myNumber.value = false
+  inputNumber.value = ""
+
+}
 
 </script>
